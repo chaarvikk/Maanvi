82231352e272e1c818fab5d2075ee6981653473a
chaarvikk <64976954+chaarvikk@users.noreply.github.com>
2020-05-29 12:41:25 +0530

    Create 1revision.java

---
+++ 1revision.java
@@ -0,0 +1,53 @@
+public class revison {
+
+	public static void main (String[] args) { 
+/*switch lesson!!!!!!!	
+		int day = 4 g;
+		switch	(day) {
+	
+		case 1:
+			System.out.println("Monday");
+		break;
+		
+		case 2 :
+			System.out.println("Tuesday");
+		break;
+		
+		case 3 :
+			System.out .println("Wednesday");
+		break;
+		
+		case 4 :
+			System.out.println("Thursday");
+		break;
+		
+		case 5 :
+			System.out.println("Friday");
+		break;
+			
+		case 6 :
+			System.out.println("Saturday");
+		break;
+		
+		case 7 :
+			System.out.println("Sunday");
+		break;*/
+		
+		/* While Loop lesson!!!!!!
+			
+				//I am not going to write the main method//
+						int i = 0;
+				 		while (i < 100000) {
+				 	System.out.println(i);
+				 	i++;	
+				 		}*/
+		//Loop lesson!!!!!!!!//
+		
+		for (int i = 0 ; i <= 10; i++){
+		System.out.println( "I " +i + " Cockroach");
+		}
+		
+		}	
+	}
+
+
